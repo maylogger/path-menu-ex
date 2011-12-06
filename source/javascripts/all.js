2f3f17e0b171bd6635740358521756431e43e3da
evenwu <evendesign@gmail.com>
2011-12-06 22:48:43 +0800

    add icons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,13 +1,11 @@
 $(function(){
   $(".plus").toggle(function(){
-    event.preventDefault();
     $(this).addClass("plus-on");
     $(".path").addClass("path-on");
     $(".item:last").bind('animationend oAnimationEnd webkitAnimationEnd', function() {
       $(".path").addClass("path-on-end").removeClass("path-on").removeClass("path-off-end");
     });
   },function(){
-    event.preventDefault();
     $(this).removeClass("plus-on");
     $(".path").addClass("path-off");
     $(".item:first").bind('animationend oAnimationEnd webkitAnimationEnd', function() {
